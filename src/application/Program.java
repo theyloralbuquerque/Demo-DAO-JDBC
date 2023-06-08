1b80cc63e8234169483f6df25402f04fbe0f2f67
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-06-07 22:07:19 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -10,9 +13,16 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerdao(); // Chama o método que cria um Dao e armazena em sellerDao.
 		
-		System.out.println("====TEST 01====");
+		System.out.println("====TEST 01: Seller findById====");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);	
+		
+		System.out.println("\n====TEST 02: Seller findByDepartment====");
+		Department department = new Department(2, null);   // Criação da variável department do tipo department recebendo o id 2 e o name null.
+		List<Seller> list = sellerDao.findByDepartment(department); // Criação da Lista list do tipo Seller recebendo o retorno do método .findByDepartment().
+		for (Seller obj : list) {    // for each para exibir os elementos da Lista list.
+			System.out.println(obj);
+		}
 	}
 }
